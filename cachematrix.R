49515364c6123925d95088ebb8084db2a54891a2
smdexter <kaloyan.velichkov@gmail.com>
2015-09-27 19:37:09 +0300

    Week2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creating a list which represents matrix and it's inverted value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function (y){
+    x<<-y
+    i<<-NULL
+  }
+  get<-function()x
+  setinv <-function (inv) i <<- inv
+  getinv<- function()i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
 }
 
 
@@ -12,4 +22,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i<-x$getinv()
+  if (!is.null(i)) {
+    message("cash")
+    return(i)
+  }
+  data<-x$get()
+  i<-solve(data,...)
+    x$setinv(i)
+    message("solved")
+    i
+  
+  
 }
+
